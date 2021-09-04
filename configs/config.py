408f4de333daf254a665c26395087a2c66582511
justUndertaker <806792561@qq.com>
2021-09-04 12:33:54 +0800

    :zap:修改配置提示信息

--- configs/config.py
+++ configs/config.py
@@ -2,12 +2,38 @@
 全局配置文件
 '''
 
-
-# 插件日志的debug信息是否显示在控制台
 LOGGER_DEBUG: bool = True
+'''插件日志的debug信息是否显示在控制台'''
 
-# 默认绑定服务器
+
 DEFAULT_SERVER: str = "幽月轮"
+'''默认绑定服务器'''
 
-# 群默认开启状态
+
 DEFAULT_STATUS: bool = True
+'''群默认开启状态'''
+
+
+ROBOT_ACTIVE: int = 20
+'''机器人自动插话活跃度，1-100'''
+
+
+CHAT_NLP = {
+    "secretId": "",  # 腾讯云API的secretId，开通地址：https://console.cloud.tencent.com/cam/capi
+    "secretKey": ""  # 腾讯云API的secretKey，NLP申请：https://console.cloud.tencent.com/nlp
+}
+'''聊天配置-自动聊天'''
+
+
+CHAT_VOICE = {
+    "appkey": "",  # 阿里云的语音接口，开通地址：https://usercenter.console.aliyun.com/
+    "access": "",  # 阿里云的语音接口，NLP申请：https://nls-portal.console.aliyun.com/overview
+    "secret": "",  # 阿里云的语音接口
+    "voice": "Aitong",  # 发言人，文档：https://help.aliyun.com/knowledge_detail/84435.html?spm=a2c4g.11186631.2.1.67045663WlpL4n
+    "format": "mp3",  # 编码格式，支持 PCM WAV MP3
+    "sample_rate": 16000,  # 采样率
+    "volume": 50,  # 音量，取值范围：0～100
+    "speech_rate": 0,  # 语速，取值范围：-500～500
+    "pitch_rate": 0,  # 音调，取值范围：-500～500
+}
+'''聊天配置-语音合成'''
--- configs/pathConfig.py
+++ configs/pathConfig.py
@@ -2,16 +2,20 @@
 '''
 此文件用于保存各个路径的配置
 '''
-# 数据库位置
+
 DATABASE_PATH = './database/data.db'
+'''数据库位置'''
 
-# 日志位置
+
 LOG_PATH = './log/'
+'''日志位置'''
 
-# 字体位置
+
 PATH_FONT = './resources/font/msyh.ttc'
+'''字体位置'''
 
 
-# 功能菜单-开关图片
 MEAU_IMG_OPEN = './resources/img/plugins_manager/open.png'
+'''功能菜单-开关图片'''
 MEAU_IMG_CLOSE = './resources/img/plugins_manager/close.png'
+'''功能菜单-开关图片'''
