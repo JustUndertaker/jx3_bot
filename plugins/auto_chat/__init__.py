2b4f19cd34a1d149bd9ecd36355f010aa52ec7e3
justUndertaker <806792561@qq.com>
2021-09-04 13:45:13 +0800

    :sparkles:增加自动插话功能

---
+++ plugins/auto_chat/__init__.py
@@ -0,0 +1,44 @@
+from nonebot.adapters.cqhttp import Bot, GroupMessageEvent, MessageSegment
+from nonebot.adapters.cqhttp.permission import GROUP
+from nonebot import on_message
+from nonebot.plugin import export
+import random
+from .data_source import (
+    get_active,
+    get_saohua,
+    get_voice,
+)
+
+export = export()
+export.plugin_name = '自动插话'
+export.plugin_usage = '让机器人自动插话。'
+export.ignore = False  # 插件管理器忽略此插件
+
+
+message = on_message(permission=GROUP, priority=99, block=True)
+
+
+@message.handle()
+async def _(bot: Bot, event: GroupMessageEvent):
+    '''
+    处理自动插话
+    '''
+    # 随机是否插话
+    num = random.randrange(100)
+    active = await get_active(event.group_id)
+    if num > active:
+        await message.finish()
+
+    user_id = event.user_id
+    # 获取一条骚话
+    text = await get_saohua()
+    num = random.randrange(100)
+    if num < 20:
+        # 转换语音
+        voice_str = await get_voice(text)
+        if voice_str is not None:
+            msg = MessageSegment.record(voice_str)
+            await message.finish(msg)
+
+    msg = MessageSegment.at(user_id)+MessageSegment.text(text)
+    await message.finish(msg)
---
+++ plugins/auto_chat/data_source.py
@@ -0,0 +1,87 @@
+from modules.group_info import GroupInfo
+from utils.user_agent import get_user_agent
+from utils.log import logger
+from configs.config import CHAT_VOICE
+import httpx
+from typing import Optional
+import base64
+
+
+async def get_active(group_id: int) -> int:
+    '''
+    :说明
+        获取群里机器人的活跃值
+
+    :参数
+        * group_id：QQ群号
+
+    :返回
+        * int：活跃度
+    '''
+    await GroupInfo.get_active(group_id)
+
+
+async def get_saohua() -> str:
+    '''
+    获取一条骚话
+    '''
+    url = 'https://www.jx3api.com/app/random'
+    async with httpx.AsyncClient(headers=get_user_agent()) as client:
+        try:
+            req_url = await client.get(url=url)
+            req = req_url.json()
+            if req['code'] == 200:
+                data = req['data']
+                msg = data['text']
+                log = f'请求骚话成功！内容：{msg}'
+                logger.debug(log)
+                return msg
+        except Exception as e:
+            log = f'请求骚话失败，原因：{e}'
+            logger.debug(log)
+            # 请求失败，从本地返回一条
+            msg = "你好骚啊。"
+            return msg
+
+
+async def get_voice(text: str) -> Optional[str]:
+    '''
+    :说明
+        文本转语音
+
+    :参数
+        * text：转换内容
+
+    :返回
+        * `str`：base64编码内容
+        * `None`：转换出错
+    '''
+    # 判断配置是否完整
+    if CHAT_VOICE['appkey'] == "" or CHAT_VOICE['access'] == "" or CHAT_VOICE['secret'] == "":
+        return None
+    log = f'请求语音合成：{text}'
+    logger.debug(log)
+    url = 'https://www.jx3api.com/extend/aliyun'
+    params = CHAT_VOICE.copy()
+    params['text'] = text
+    async with httpx.AsyncClient(headers=get_user_agent()) as client:
+        try:
+            req_url = await client.get(url=url)
+            req = req_url.json()
+            if req['code'] == 200:
+                data = req['data']
+                voice_url = data['url']
+                # 获取语音数据
+                req_url = await client.get(url=voice_url)
+                byte_data = req_url.content
+                base64_str = base64.b64encode(byte_data)
+                req_str = 'base64://'+base64_str.decode()
+                return req_str
+            else:
+                log = f'语音合成请求参数出错。'
+                logger.debug(log)
+                return None
+        except Exception as e:
+            log = f'请求链接失败，原因：{e}'
+            logger.error(log)
+            return None
