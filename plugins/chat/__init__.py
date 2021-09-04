bb5547a1dce886056ed7f0e0c6ca90bee0ccd3e1
justUndertaker <806792561@qq.com>
2021-09-04 14:16:36 +0800

    :sparkles:加入智能闲聊功能

---
+++ plugins/chat/__init__.py
@@ -0,0 +1,42 @@
+from nonebot.adapters.cqhttp import Bot, GroupMessageEvent, MessageSegment
+from nonebot.adapters.cqhttp.permission import GROUP
+from nonebot.rule import to_me
+from nonebot import on_message
+from nonebot.plugin import export
+from utils.log import logger
+from .data_source import get_reply_jx3, get_reply_qingyunke
+
+export = export()
+export.plugin_name = '智能闲聊'
+export.plugin_usage = '让机器人开启聊天功能。'
+export.ignore = False  # 插件管理器忽略此插件
+
+
+chat = on_message(rule=to_me(), permission=GROUP, priority=8, block=True)
+
+
+@chat.handle()
+async def _(bot: Bot, event: GroupMessageEvent):
+    '''
+    自动根据确定API
+    '''
+    # 获得聊天内容
+    text = event.get_plaintext()
+    name = event.sender.nickname if event.sender.card == "" else event.sender.card
+    log = f'{name}（{event.user_id}，{event.group_id}）闲聊：{text}'
+    logger.info(log)
+
+    # 使用jx3api访问
+    msg = await get_reply_jx3(text)
+    if msg is None:
+        # 使用青云客访问
+        msg = await get_reply_qingyunke(text)
+        if msg is None:
+            # 访问失败
+            log = f'接口访问失败，关闭事件。'
+            logger.info(log)
+            await chat.finish()
+
+    log = f'接口返回：{msg}'
+    logger.info(log)
+    await chat.finish(msg)
---
+++ plugins/chat/data_source.py
@@ -0,0 +1,82 @@
+from utils.log import logger
+from configs.config import CHAT_NLP
+import httpx
+from utils.user_agent import get_user_agent
+from utils.utils import nickname
+from typing import Optional
+
+
+async def get_reply_jx3(question: str) -> Optional[str]:
+    '''
+    使用jx3_api获取回复
+    '''
+    # 判断参数是否完整
+    if CHAT_NLP['secretId'] == "" or CHAT_NLP['secretKey'] == "":
+        log = f'jx3_api接口参数不足，无法请求。'
+        logger.debug(log)
+        return None
+
+    url = "https://www.jx3api.com/extend/nlpchat"
+    params = CHAT_NLP.copy()
+    params['name'] = nickname
+    params['question'] = question
+    async with httpx.AsyncClient(headers=get_user_agent()) as client:
+        try:
+            req_url = await client.get(url=url, params=params)
+            req = req_url.json()
+            if req['code'] == 200:
+                log = 'jx3API请求成功。'
+                logger.debug(log)
+                data = req['data']
+                return data['answer']
+            else:
+                log = f'jx3API请求失败：{req["msg"]}'
+                logger.debug(log)
+                return None
+        except Exception as e:
+            log = f'API访问失败：{e}'
+            logger.error(log)
+            return None
+
+
+async def get_reply_qingyunke(text: str) -> Optional[str]:
+    '''
+    :说明
+        获取聊天结果，使用青云客的API，备胎
+
+    :参数
+        * text：聊天内容
+
+    :返回
+        * str：聊天结果
+
+    :异常
+        * NetworkError, Exception
+    '''
+    params = {
+        'key': 'free',
+        'appid': 0,
+        'msg': text
+    }
+    url = 'http://api.qingyunke.com/api.php'
+    async with httpx.AsyncClient(headers=get_user_agent()) as client:
+        try:
+            req_url = await client.get(url, params=params)
+            req = req_url.json()
+            if req['result'] == 0:
+                msg = str(req['content'])
+                # 消息替换
+                msg = msg.replace(r'{br}', '\n')
+                msg = msg.replace('菲菲', nickname)
+                log = f'请求青云客API成功。'
+                logger.debug(log)
+                return msg
+            else:
+                e = req['content']
+                log = f'青云客API请求失败：{e}'
+                logger.error(log)
+                return None
+        except Exception as e:
+            log = f'青云客API访问失败：{e}'
+            logger.error(log)
+            return None
