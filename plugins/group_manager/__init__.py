54d1c8db5f205dcb5207b1c36e2d078435971ade
justUndertaker <806792561@qq.com>
2021-09-04 11:34:54 +0800

    :art:增加绑定服务器功能

--- plugins/group_manager/__init__.py
+++ plugins/group_manager/__init__.py
@@ -1,7 +1,14 @@
-from nonebot import get_driver
+from nonebot import get_driver, on_regex
 from nonebot.plugin import export
-from nonebot.adapters.cqhttp import Bot
-from .data_source import group_init
+from nonebot.adapters.cqhttp import Bot, GroupMessageEvent
+from nonebot.permission import SUPERUSER
+from nonebot.adapters.cqhttp.permission import GROUP_ADMIN, GROUP_OWNER
+from utils.log import logger
+from .data_source import (
+    group_init,
+    get_server_name,
+    change_server,
+)
 
 export = export()
 export.plugin_name = '群管理'
@@ -20,3 +27,31 @@ async def _(bot: Bot):
     group_list = await bot.get_group_list()
     for group in group_list:
         await group_init(group['group_id'])
+
+server_regex = r"^绑定服务器 [\u4e00-\u9fa5]+$"
+server_useage = "[更换绑定服务器]\n群管理命令：绑定服务器 XXX"
+server_change = on_regex(pattern=server_regex, permission=SUPERUSER | GROUP_OWNER | GROUP_ADMIN, priority=2, block=True)
+
+
+@server_change.handle()
+async def _(bot: Bot, event: GroupMessageEvent):
+    '''
+    管理员绑定服务器
+    '''
+    regex = event.get_plaintext()
+    name = regex.split(' ')[-1]
+    playername = event.sender.nickname if event.sender.card == "" else event.sender.card
+    group_id = event.group_id
+    log = f'[{playername}]({event.user_id},{group_id})更换服务器，参数：{name}'
+    logger.info(log)
+    server = await get_server_name(name)
+    if server is None:
+        msg = f'参数错误，检查一下呀。\n\n{server_useage}'
+        log = f'更换服务器出错，参数错误。'
+        logger.info(log)
+        await server_change.finish(msg)
+    await change_server(group_id=group_id, server=server)
+    msg = f'当前绑定服务器为：{server}'
+    log = f'更换服务器成功，绑定服务器为：{server}'
+    logger.info(log)
+    await server_change.finish(msg)
--- plugins/group_manager/data_source.py
+++ plugins/group_manager/data_source.py
@@ -1,4 +1,7 @@
 from modules.group_info import GroupInfo
+from typing import Optional
+import httpx
+from utils.user_agent import get_user_agent
 
 
 async def group_init(group_id: int) -> None:
@@ -6,3 +9,32 @@ async def group_init(group_id: int) -> None:
     注册群信息
     '''
     await GroupInfo.append_or_update(group_id)
+
+
+async def change_server(group_id: int, server: str) -> None:
+    '''
+    :说明
+        更换绑定服务器
+
+    :参数
+        * group_id：QQ群号
+        * server：服务器
+    '''
+    await GroupInfo.set_server(group_id, server)
+
+
+async def get_server_name(name: str) -> Optional[str]:
+    '''
+    根据服务器获取主服务器名
+    '''
+    url = 'https://www.jx3api.com/app/master'
+    params = {"name": name}
+    async with httpx.AsyncClient(headers=get_user_agent()) as client:
+        req_url = await client.get(url=url, params=params)
+        req = req_url.json()
+    code = req.get('code')
+    if code == 200:
+        data = req['data']
+        return data['server']
+    else:
+        return None
