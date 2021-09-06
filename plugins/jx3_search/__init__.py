c8d1b681f1c7f5016e90714d8c8f846a674f7112
justUndertaker <806792561@qq.com>
2021-09-07 01:40:41 +0800

    :zap:增加命令触发方式

--- plugins/jx3_search/__init__.py
+++ plugins/jx3_search/__init__.py
@@ -1,10 +1,18 @@
 from nonebot.adapters.cqhttp import Bot, GroupMessageEvent
 from nonebot.adapters.cqhttp.permission import GROUP
-from .data_source import get_server, get_macro_name, get_xinfa
 from nonebot.plugin import export
 from nonebot import on_regex
 import time
 from utils.jx3_soket import send_ws_message
+from .data_source import (
+    get_server,
+    get_macro_name,
+    get_xinfa,
+    get_qixue_name,
+    get_medicine_name,
+    get_peizhuang_name,
+    get_gonglue_name
+)
 
 
 export = export()
@@ -14,12 +22,18 @@ export.ignore = False  # 插件管理器忽略此插件
 
 
 daily = on_regex(pattern=r'^日常$', permission=GROUP, priority=5, block=True)  # 日常查询
-equipquery = on_regex(pattern=r'^装备 [\u4e00-\u9fa5]+$', permission=GROUP, priority=5, block=True)  # 装备查询
+equipquery = on_regex(pattern=r'^(装备)|(属性) [\u4e00-\u9fa5]+$', permission=GROUP, priority=5, block=True)  # 装备查询
 open_server_send = on_regex(pattern=r'^开服$', permission=GROUP, priority=5, block=True)  # 开服查询
 gold_query = on_regex(pattern=r'^金价$', permission=GROUP, priority=5, block=True)  # 金价查询
-extra_point = on_regex(pattern=r'^奇穴 [\u4e00-\u9fa5]+$', permission=GROUP, priority=5, block=True)  # 奇穴查询
-medicine = on_regex(pattern=r'^小药 [\u4e00-\u9fa5]+$', permission=GROUP, priority=5, block=True)  # 小药查询
-equip_group_query = on_regex(pattern=r'^配装 [\u4e00-\u9fa5]+$', permission=GROUP, priority=5, block=True)  # 配装查询
+
+extra_point_regex = r"(^奇穴 [\u4e00-\u9fa5]+$)|(^[\u4e00-\u9fa5]+奇穴$)"
+extra_point = on_regex(pattern=extra_point_regex, permission=GROUP, priority=5, block=True)  # 奇穴查询
+
+medicine_regex = r"(^小药 [\u4e00-\u9fa5]+$)|(^[\u4e00-\u9fa5]+小药$)"
+medicine = on_regex(pattern=medicine_regex, permission=GROUP, priority=5, block=True)  # 小药查询
+
+equip_group_query_regex = r"(^配装 [\u4e00-\u9fa5]+$)|(^[\u4e00-\u9fa5]+配装$)"
+equip_group_query = on_regex(pattern=equip_group_query_regex, permission=GROUP, priority=5, block=True)  # 配装查询
 
 macro_regex = r"(^宏 [\u4e00-\u9fa5]+$)|(^[\u4e00-\u9fa5]+宏$)"
 macro = on_regex(pattern=macro_regex, permission=GROUP, priority=5, block=True)  # 宏查询
@@ -28,7 +42,9 @@ adventure_regex = r"^(前置)|(条件) [\u4e00-\u9fa5]+$"
 adventurecondition = on_regex(pattern=adventure_regex, permission=GROUP, priority=5, block=True)  # 奇遇前置查询
 
 exam = on_regex(pattern=r"^(考试)|(科举) ", permission=GROUP, priority=5, block=True)  # 科举查询
-raiderse = on_regex(pattern=r"^攻略 [\u4e00-\u9fa5]+$", permission=GROUP, priority=5, block=True)  # 攻略查询
+
+raiderse = r"(^攻略 [\u4e00-\u9fa5]+$)|(^[\u4e00-\u9fa5]+攻略$)"
+raiderse = on_regex(pattern=raiderse, permission=GROUP, priority=5, block=True)  # 攻略查询
 pendant = on_regex(pattern=r'^挂件 [\u4e00-\u9fa5]+$', permission=GROUP, priority=5, block=True)  # 挂件查询
 # raiderse_search = pendant = on_regex(pattern=r'^奇遇 [\u4e00-\u9fa5]+$', permission=GROUP, priority=5, block=True)  # 奇遇查询
 # TODO：条件查询，器物谱查询，装饰查询，挂件查询，装备属性
@@ -36,6 +52,7 @@ pendant = on_regex(pattern=r'^挂件 [\u4e00-\u9fa5]+$', permission=GROUP, prior
 
 @daily.handle()
 async def _(bot: Bot, event: GroupMessageEvent):
+    '''日常查询'''
     echo = int(time.time())
     group_id = event.group_id
     server = await get_server(group_id)
@@ -50,6 +67,7 @@ async def _(bot: Bot, event: GroupMessageEvent):
 
 @equipquery.handle()
 async def _(bot: Bot, event: GroupMessageEvent):
+    '''装备查询'''
     echo = int(time.time())
     group_id = event.group_id
     server = await get_server(group_id)
@@ -99,7 +117,8 @@ async def _(bot: Bot, event: GroupMessageEvent):
     '''奇穴查询'''
     echo = int(time.time())
     group_id = event.group_id
-    get_name = event.get_plaintext().split(" ")[-1]
+    text = event.get_plaintext()
+    get_name = get_qixue_name(text)
     name = get_xinfa(get_name)
     msg = {
         "type": 1008,
@@ -115,7 +134,8 @@ async def _(bot: Bot, event: GroupMessageEvent):
     '''小药查询'''
     echo = int(time.time())
     group_id = event.group_id
-    get_name = event.get_plaintext().split(" ")[-1]
+    text = event.get_plaintext()
+    get_name = get_medicine_name(text)
     name = get_xinfa(get_name)
     msg = {
         "type": 1010,
@@ -193,7 +213,8 @@ async def _(bot: Bot, event: GroupMessageEvent):
     '''配装查询'''
     echo = int(time.time())
     group_id = event.group_id
-    get_name = event.get_plaintext().split(" ")[-1]
+    text = event.get_plaintext()
+    get_name = get_peizhuang_name(text)
     name = get_xinfa(get_name)
     msg = {
         "type": 1006,
@@ -209,7 +230,8 @@ async def _(bot: Bot, event: GroupMessageEvent):
     '''攻略查询'''
     echo = int(time.time())
     group_id = event.group_id
-    get_name = event.get_plaintext().split(" ")[-1]
+    text = event.get_plaintext()
+    get_name = get_gonglue_name(text)
     msg = {
         "type": 1023,
         "name": get_name,
--- plugins/jx3_search/data_source.py
+++ plugins/jx3_search/data_source.py
@@ -19,6 +19,42 @@ def get_macro_name(text: str) -> str:
         return text.split(' ')[-1]
 
 
+def get_qixue_name(text: str) -> str:
+    '''奇穴查询返回职业'''
+    args = re.search(r'^[\u4e00-\u9fa5]+奇穴$', text)
+    if args is not None:
+        return text[:-2]
+    else:
+        return text.split(' ')[-1]
+
+
+def get_medicine_name(text: str) -> str:
+    '''小药查询返回职业'''
+    args = re.search(r'^[\u4e00-\u9fa5]+小药$', text)
+    if args is not None:
+        return text[:-2]
+    else:
+        return text.split(' ')[-1]
+
+
+def get_peizhuang_name(text: str) -> str:
+    '''配装查询返回职业'''
+    args = re.search(r'^[\u4e00-\u9fa5]+配装$', text)
+    if args is not None:
+        return text[:-2]
+    else:
+        return text.split(' ')[-1]
+
+
+def get_gonglue_name(text: str) -> str:
+    '''攻略查询返回'''
+    args = re.search(r'^[\u4e00-\u9fa5]+攻略$', text)
+    if args is not None:
+        return text[:-2]
+    else:
+        return text.split(' ')[-1]
+
+
 def get_xinfa(name: str) -> str:
     '''获取心法名称'''
     for key, xinfa in zhiye_name.items():
