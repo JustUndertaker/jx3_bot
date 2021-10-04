11c2cde6586eed42df6f5b4edc42dbdb0d6f66c5
justUndertaker <806792561@qq.com>
2021-10-04 18:10:08 +0800

    :bug:新闻推送bug

--- src/plugins/jx3_news_recv/data_source.py
+++ src/plugins/jx3_news_recv/data_source.py
@@ -14,7 +14,7 @@ async def get_server(bot_id: int, group_id: int) -> str:
 async def get_robot_status(bot_id: int, group_id: int) -> bool:
     '''获取机器人状态'''
 
-    robot_status = await GroupInfo.get_robot_status(group_id)
+    robot_status = await GroupInfo.get_robot_status(bot_id, group_id)
     _, self_module = os.path.split(os.path.split(__file__)[0])
     plugin_status = await PluginInfo.get_status(bot_id, self_module, group_id)
 
